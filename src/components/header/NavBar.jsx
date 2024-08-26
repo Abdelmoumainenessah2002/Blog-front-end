184914e148f26d4853b5775532e159e7ef167ba4
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-26 19:08:01 +0100

    create the navLinks and make it responsive

---
+++ src/components/header/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const NavBar = ({toggle, setToggle}) => {
+  return (
+    <nav
+      style={{
+        clipPath: toggle && "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+      }}
+      className="navbar"
+    >
+      <ul className="nav-links">
+        <li onClick={() => setToggle(false)} className="nav-link">
+          <i className="bi bi-house"></i> Home
+        </li>
+        <li onClick={() => setToggle(false)} className="nav-link">
+          <i className="bi bi-stickies"></i> Posts
+        </li>
+        <li onClick={() => setToggle(false)} className="nav-link">
+          <i className="bi bi-journal-plus"></i> Create
+        </li>
+        <li onClick={() => setToggle(false)} className="nav-link">
+          <i className="bi bi-person-check"></i> Admin Dashbord
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;

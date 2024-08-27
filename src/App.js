ccd832fe8c150f82b0e86f3dc905be38201bf48a
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-27 13:58:51 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from "./components/header/Header";
-
+import Home from "./pages/home/Home";
+import Login from "./pages/forms/Login";
+import Register from "./pages/forms/Register";
+import PostsPage from "./pages/posts-page/PostsPage";
+import CreatePost from "./pages/create-post/CreatePost";
+import AdminDashbord from "./pages/admin/AdminDashbord";
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes >
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="about" element={<h1>About</h1>} />
-        <Route path="services" element={<h1>Services</h1>} />
-        <Route path="contact" element={<h1>Contact</h1>} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/admin-dashbord" element={<AdminDashbord />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }

5b186cb712ac9180072ae0994405a12849288d75
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-31 16:24:07 +0100

    Add ForgotPassword component

---
+++ src/pages/forms/ForgotPassword.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  // form submit handler
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+    if (email.trim() === "") return toast.error("Email cannot be empety");
+
+    console.log({ email });
+  };
+
+  return (
+    <div>
+      <section className="form-container">
+        <div className="form-title">Reset your password now!</div>
+        <form onSubmit={formSubmitHandler} className="form">
+          <div className="form-group">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-input"
+              id="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="form-btn">
+            Send Email
+          </button>
+        </form>
+        <div className="form-footer">
+          Back To <Link to="/login">Login</Link>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ForgotPassword;

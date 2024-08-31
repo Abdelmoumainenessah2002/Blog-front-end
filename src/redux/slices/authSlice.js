bf6a3f1d881916e88ef1ca7b75df8b62afb03ec6
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-31 16:32:36 +0100

    Add authSlice for Redux

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,30 @@
+  // authSlice.js
+  import { createSlice } from "@reduxjs/toolkit";
+
+  const authSlice = createSlice({
+    name: "auth",
+    initialState: {
+      user: localStorage.getItem("userInfo") ?
+        JSON.parse(localStorage.getItem("userInfo")) : null,
+      registerMessage: null
+    },
+    reducers: {
+      login(state, action) {
+        state.user = action.payload;
+      },
+      logout(state) {
+        state.user = null;
+      },
+      register(state, action) {
+        state.registerMessage = action.payload;
+      },
+      setUserPhoto(state, action) {
+        state.user.profilePhoto = action.payload;
+      }
+    },
+  });
+
+  const authReducer = authSlice.reducer;
+  const authActions = authSlice.actions;
+
+  export { authReducer, authActions };

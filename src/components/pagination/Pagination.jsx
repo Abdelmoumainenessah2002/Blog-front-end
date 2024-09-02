f750f934e964ee198948375f6de3108032b5b594
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-09-02 02:55:42 +0100

    make the pagination dynamic

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import './pagination.css'
-function Pagination() {
+import React from "react";
+import "./pagination.css";
+
+function Pagination({ pages, currentPage, setCurrentPage }) {
+  // Destructure props correctly
+
+  const generatePages = [];
+
+  // Populate the generatePages array
+  for (let i = 1; i <= pages; i++) {
+    generatePages.push(i);
+  }
+
   return (
     <div className="pagination">
-      <div className="page previous">Prev</div>
-      {[1,2,3,4,5].map((page) => (
-        <div key={page} className="page">{page}</div>
+      <div
+        className={`page previous ${currentPage === 1 ? "disabled" : ""}`}
+        onClick={() => currentPage > 1 && setCurrentPage(currentPage - 1)}
+      >
+        Prev
+      </div>
+      {generatePages.map((page) => (
+        <div
+          onClick={() => setCurrentPage(page)}
+          key={page}
+          className={`page ${currentPage === page ? "active" : ""}`}
+        >
+          {page}
+        </div>
       ))}
-      <div className="page next">Next</div>
+      <div
+        className={`page next ${currentPage === pages ? "disabled" : ""}`}
+        onClick={() => currentPage < pages && setCurrentPage(currentPage + 1)}
+      >
+        Next
+      </div>
     </div>
   );
 }
 
-export default Pagination
+export default Pagination;

54e5d518d544a713c373fd2f7d6b6974c56b718d
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-09-02 03:00:37 +0100

    get the post details from the database

--- src/pages/post-details/PostDetails.jsx
+++ src/pages/post-details/PostDetails.jsx
@@ -1,5 +1,4 @@
-import { Link, useParams } from "react-router-dom";
-import { posts } from "../../dummyData";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "./post-details.css";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
@@ -7,31 +6,49 @@ import AddComment from "../../components/comments/AddComment";
 import CommentList from "../../components/comments/CommentList";
 import Swal from "sweetalert2";
 import UpdatePostModel from "./UpdatePostModel";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deletePost,
+  fetchSinglePost,
+  toggleLikePost,
+  updatePostImage,
+} from "../../redux/apiCalls/postsApiCall";
 
 const PostDetails = () => {
+  const dispatch = useDispatch();
+  const { post } = useSelector((state) => state.post);
+
+  const { user } = useSelector((state) => state.auth);
+
   const { id } = useParams();
-  const post = posts.find((post) => post._id === parseInt(id));
 
   const [file, setFile] = useState(null);
   const [updatePost, setUpdatePost] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [id]);
+    dispatch(fetchSinglePost(id));
+  }, [id, dispatch]);
 
   const updateImageSubmitHandler = (e) => {
     e.preventDefault();
     if (!file) return toast.error("there is no file uploaded!");
 
-    console.log("Image Updated");
+    const formData = new FormData();
+
+    formData.append("image", file);
+
+    dispatch(updatePostImage(formData, post?._id));
   };
+
+  // navigate to profile
+  const navigate = useNavigate();
 
   // Delete post handler
   const deletePostHandler = () => {
     Swal.fire({
       title: "Are you sure?",
-      text: "You won't be able to revert this!",
+      text: "You won't be able to revert this post!",
       icon: "warning",
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
@@ -39,80 +56,85 @@ const PostDetails = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire({
-          title: "Deleted!",
-          text: "post has been deleted.",
-          icon: "success",
-        });
+        dispatch(deletePost(post?._id));
+        navigate(`/profile/${user?._id}`);
       }
     });
-  }
+  };
 
   return (
     <section className="post-details">
       <div className="post-details-image-wrapper">
         <img
-          src={file ? URL.createObjectURL(file) : post.image}
+          src={file ? URL.createObjectURL(file) : post?.image.url}
           alt=""
           className="post-details-image"
         />
-        <form
-          className="update-post-image-form"
-          onSubmit={updateImageSubmitHandler}
-        >
-          <label htmlFor="file" className="update-post-label">
-            <i className="bi bi-image-fill"></i>
-            Select New Image
-          </label>
-          <input
-            type="file"
-            name="file"
-            id="file"
-            style={{ display: "none" }}
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <button type="submit">Upload</button>
-        </form>
+        {user?._id === post?.user._id && (
+          <form
+            className="update-post-image-form"
+            onSubmit={updateImageSubmitHandler}
+          >
+            <label htmlFor="file" className="update-post-label">
+              <i className="bi bi-image-fill"></i>
+              Select New Image
+            </label>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+            <button type="submit">Upload</button>
+          </form>
+        )}
       </div>
-      <h1 className="post-details-title">{post.title}</h1>
+      <h1 className="post-details-title">{post?.title}</h1>
       <div className="post-details-user-info">
         <img
-          src={post.user.image}
-          alt={`${post.user.username} avatar`}
+          src={post?.user.profilePhoto.url}
+          alt={`${post?.user.username} avatar`}
           className="post-details-user-image"
         />
         <div className="post-details-user">
           <strong>
-            <Link to={`/profile/1`}>{post.user.username}</Link>
+            <Link to={`/profile/${post?.user._id}`}>{post?.user.username}</Link>
           </strong>
-          <span>{post.createdAt}</span>
+          <span>{new Date(post?.createdAt).toDateString()}</span>
         </div>
       </div>
-      <p className="post-details-description">
-        {post.description} Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Incidunt consequuntur quae rerum eum sapiente nihil quam. Autem
-        esse corporis dolore ab id, necessitatibus tempore, commodi, magnam
-        soluta quas numquam quam.Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Incidunt consequuntur quae rerum eum sapiente nihil
-        quam. Autem esse corporis dolore ab id, necessitatibus tempore, commodi,
-        magnam soluta quas numquam quam.
-      </p>
+      <p className="post-details-description">{post?.description}</p>
       <div className="post-details-icon-wrapper">
         <div className="">
-          <i className="bi bi-hand-thumbs-up"></i>
-          <small>{post.likes.length} likes</small>
+          {user && (
+            <i
+              onClick={() => dispatch(toggleLikePost(post?._id))}
+              className={
+                post?.likes.includes(user._id)
+                  ? "bi bi-hand-thumbs-up-fill"
+                  : "bi bi-hand-thumbs-up"
+              }
+            ></i>
+          )}
+
+          <small>{post?.likes.length} likes</small>
         </div>
-        <div className="">
-          <i
-            onClick={() => setUpdatePost(true)}
-            className="bi bi-pencil-square"
-          ></i>
-          <i onClick={deletePostHandler} className="bi bi-trash-fill"></i>
-        </div>
+        {user?._id === post?.user._id && (
+          <div className="">
+            <i
+              onClick={() => setUpdatePost(true)}
+              className="bi bi-pencil-square"
+            ></i>
+            <i onClick={deletePostHandler} className="bi bi-trash-fill"></i>
+          </div>
+        )}
       </div>
       <AddComment />
-      <CommentList />
-      {updatePost && <UpdatePostModel post={post} setUpdatePost={setUpdatePost} />}
+      <CommentList comments={post?.comments} />
+      {updatePost && (
+        <UpdatePostModel post={post} setUpdatePost={setUpdatePost} />
+      )}
     </section>
   );
 };

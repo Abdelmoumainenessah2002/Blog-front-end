765eff33f99696b526696069c1fbc30c5c67e3ae
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-09-02 18:17:45 +0100

    get the posts of the category and put it

--- src/pages/category/Category.jsx
+++ src/pages/category/Category.jsx
@@ -1,24 +1,45 @@
-import React, { useEffect } from 'react'
-import "./category.css"
-import { useParams } from 'react-router-dom'
-import PostList from '../../components/posts/PostList'
-import {posts} from '../../dummyData'
+import React, { useEffect } from "react";
+import "./category.css";
+import { useParams, Link } from "react-router-dom";
+import PostList from "../../components/posts/PostList";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPostsBasedOnCategory } from "../../redux/apiCalls/postsApiCall";
 
 function Category() {
-    const { category } = useParams();
+  // Dispatch function
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        window.scrollTo(0,0)
-    }, []);
+  // selector function
+  const { postsCate } = useSelector((state) => state.post);
+
+  // Get posts from the link
+  const { category } = useParams();
+
+
+  // move to the top of the page
+  useEffect(() => {
+    dispatch(fetchPostsBasedOnCategory(category));
+    window.scrollTo(0, 0);
+  }, [category]);
 
   return (
     <div>
       <section className="category">
-        <h1 className="category-title">Posts Based On {category}</h1>
-        <PostList posts={posts} />
+        {postsCate.length === 0 ? (
+          <div className="no-posts">
+            <h2>
+              No posts with <span>{category}</span> category!!
+            </h2>
+            <Link to="/" className="btn">
+              Go back to home
+            </Link>
+          </div>
+        ) : (
+          <PostList posts={postsCate} />
+        )}
       </section>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
